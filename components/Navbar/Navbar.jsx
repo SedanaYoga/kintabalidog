2ac48829dbfc1cd3c45fc377faa857823bab2d36
Sedana Yoga <9g.yoga27@gmail.com>
2022-04-24 17:59:53 +0800

    fix: navbar link with next/link

--- components/Navbar/Navbar.jsx
+++ components/Navbar/Navbar.jsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
 
@@ -5,20 +6,32 @@ const NavBar = () => {
   return (
     <Navbar bg='light' className='p-0 m-0' expand='lg'>
       <Container>
-        <Navbar.Brand>
-          <img src='/images/logo.png' alt='logo' width='60px' />
-        </Navbar.Brand>
+        <Link href='/'>
+          <a className='nav-brand'>
+            <img role='button' src='/images/logo.png' alt='logo' width='60px' />
+          </a>
+        </Link>
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav '>
           <Nav className='me-auto w-100 d-flex justify-content-between'>
             <div className='d-flex flex-lg-row flex-column flex-grow-1 justify-content-end'>
-              <Nav.Link>home</Nav.Link>
-              <Nav.Link>about us</Nav.Link>
-              <Nav.Link>browse</Nav.Link>
+              <Link href='/'>
+                <a className='nav-link'>home</a>
+              </Link>
+              <Link href='/aboutus'>
+                <a className='nav-link'>about us</a>
+              </Link>
+              <Link href='/browse'>
+                <a className='nav-link'>browse</a>
+              </Link>
             </div>
             <div className='d-flex flex-lg-row flex-column flex-grow-1 justify-content-end'>
-              <Nav.Link>login</Nav.Link>
-              <Nav.Link>register</Nav.Link>
+              <Link href='/login'>
+                <a className='nav-link'>login</a>
+              </Link>
+              <Link href='/register'>
+                <a className='nav-link'>register</a>
+              </Link>
             </div>
           </Nav>
         </Navbar.Collapse>
